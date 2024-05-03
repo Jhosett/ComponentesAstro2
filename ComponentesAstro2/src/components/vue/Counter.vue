ffb3497137a4cecc65ab080ac198d844b8f7548a
Jhosett <darwinjhosettbermudezromero@gmail.com>
2024-05-03 17:09:38 -0500

    Practica vue

---
+++ ComponentesAstro2/src/components/vue/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+let counter = ref(5)
+
+function add() {
+    counter.value++
+}
+</script>
+
+<template>
+    <div @click="add" class="bg-blue-500 rounded py-1 px-2 max-w-40 text-center text-white hover:cursor-pointer">
+        Número de clicks: {{ counter }}
+    </div>
+</template>
